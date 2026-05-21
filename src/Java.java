43fd2e3d0ac378f7b1b56f1e58df43bf04fe91d6
mearvk <mearvk@outlook.com>
2026-05-21 11:52:50 -0400

    Latest...

--- src/Java.java
+++ src/Java.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+
 public class Java
 {
-    public static void main(String...args)
+    public static void main(String...args) throws IOException
     {
         Terminal terminal = new Terminal();
 
         terminal.initialize();
+
+        //
+
+        Runtime.getRuntime().exec("cmd /c start cmd.exe");
+
     }
 }
