c073bb51ac6cbb1739674a9ed149e63415ed1e04
mearvk <mearvk@outlook.com>
2026-05-21 15:06:51 -0400

    Latest...

--- scripts/scanner.sh
+++ scripts/scanner.sh
@@ -2,10 +2,23 @@
 
 read -sp "Enter administrator password: " PASSWORD
 
+echo "1. Updating Linux System \n\r"
 echo $PASSWORD | sudo -S apt-get update
+
+echo "2. Upgrading Linux System \n\r"
 echo $PASSWORD | sudo -S apt-get upgrade
+
+echo "3. Installing ClamAV on Linux System \n\r"
 echo $PASSWORD | sudo -S apt install -y clamav clamav-daemon
+
+echo "4. Stopping & Configuring ClamAV on Linux System \n\r"
 echo $PASSWORD | sudo -S systemctl stop clamav-freshclam
+
+echo "5. Updating ClamAV on Linux System \n\r"
 echo $PASSWORD | sudo -S freshclam
+
+echo "6. Starting ClamAV on Linux System \n\r"
 echo $PASSWORD | sudo -S systemctl start clamav-freshclam
+
+echo "7. Starting Full System Scan on Linux System \n]r"
 echo $PASSWORD | sudo -S clamscan -r -i --exclude-dir=\"^/sys\" / > clamav_scan_results.legal.lsat.txt
